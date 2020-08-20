75799ec9c4974f6ac8cf4705eb01e251829d867a
Rsja12 <asocarras10@gmail.com>
2020-08-20 18:03:57 -0400

    refactor to use StyleSheet object

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
-        <TextInput 
-          placeholder='Enter your goal' 
-          style={{width: '80%', borderBottomColor: 'black', borderWidth: 1, padding: 10}} 
-        />
-        <Button title='Add' />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter your goal" style={styles.input} />
+        <Button title="Add" />
       </View>
-      <View>
-
-      </View>
-    <StatusBar style="auto" />
+      <View></View>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  input: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
