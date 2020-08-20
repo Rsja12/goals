3763522b1e2af1a941034a33f4bf5ea7b62f023f
Rsja12 <asocarras10@gmail.com>
2020-08-20 19:17:50 -0400

    add component level state. add handleInput and handleAddButton functions

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [allGoals, setAllGoals] = useState([]);
+
+  const handleInput = (input) => {
+    setGoal(input);
+  };
+
+  const handleAddButton = () => {
+    setAllGoals((preveState) => [...preveState, goal]);
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter your goal" style={styles.input} />
-        <Button title="Add" />
-      </View>
-      <View></View>
       <StatusBar style="auto" />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your goal"
+          style={styles.input}
+          onChangeText={handleInput}
+          value={goal}
+        />
+        <Button title="Add" onPress={handleAddButton} />
+      </View>
+
+      <FlatList
+        data={allGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -29,5 +61,12 @@ const styles = StyleSheet.create({
     borderBottomColor: "black",
     borderWidth: 1,
     padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
